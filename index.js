79b7fb0cab0ddd92de66138c182d1bd4fffc5f80
John Lamansky <lamansky@users.noreply.github.com>
2018-02-04 13:59:09 +0100

    Replace `possible-function` module with `pfn`

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 'use strict'
 
-const PossibleFunction = require('possible-function')
+const pfn = require('pfn')
 
 module.exports = function rtrimArray (arr, toTrim) {
-  const shouldTrim = PossibleFunction(toTrim, Array.isArray(toTrim) ? el => toTrim.includes(el) : el => el === toTrim)
+  const shouldTrim = pfn(toTrim, Array.isArray(toTrim) ? el => toTrim.includes(el) : el => el === toTrim)
   let i
   for (i = arr.length - 1; i >= 0; i--) if (!shouldTrim(arr[i])) break
   return Array.from(arr).slice(0, i + 1)
